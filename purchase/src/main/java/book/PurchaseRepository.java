90ad3d3ad724f9a312ae37e453d04dfe2aec9792
shelleykjw <67624087+shelleykjw@users.noreply.github.com>
2021-02-24 15:33:19 +0900

    Delete purchase directory

--- purchase/src/main/java/book/PurchaseRepository.java
+++
@@ -1,8 +0,0 @@
-package book;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PurchaseRepository extends PagingAndSortingRepository<Purchase, Long>{
-
-
-}
